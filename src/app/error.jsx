b209e1d15d5ea81772d8cdb1307ed9118e5484b6
lado2008 <adamialado133@gmail.com>
2024-08-05 21:08:59 +0400

    Website Cosmetics

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,7 +1,12 @@
 "use client";
+import styles from './error.module.css'
 
 const ErrorPage = () => {
-  return <div>Error: Something happened!!!</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.text}>Error: Something happened!!!</h1>
+    </div>
+  )
 };
 
 export default ErrorPage;
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
+import styles from './not-found.module.css'
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>this page doesn't exist</p>
-      <Link href={"/"}>Return to Home</Link>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Not Found!!! <b>404</b> </h2>
+        <p>this page doesn't exist</p>
+        <Link className={styles.button} href={"/"}>Return to Home</Link>
+      </div>
     </div>
   );
 };

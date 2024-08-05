71784324d5fb7324ff6835e8b3db047a7b3cf500
lado2008 <adamialado133@gmail.com>
2024-08-05 20:28:13 +0400

    Desing About And Contact Page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,34 @@ import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <h1 className={styles.text}>this is about page</h1>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Us</h2>
+        <h1 className={styles.title}>
+          Exploring the World of Ideas and Inspiration
+        </h1>
+        <p className={styles.desc}>
+          At [Your Blog Name], we are passionate about bringing you insightful and thought-provoking content that sparks curiosity and creativity. Our mission is to provide you with a diverse range of articles on topics that matter, from the latest trends in technology to inspiring stories from around the world.
+        </p>
+        <p className={styles.desc}>
+          With a commitment to quality and a dedication to our readers, our team of writers and experts strive to deliver valuable content that informs and engages. Join us as we explore new ideas, share valuable insights, and inspire positive change through the power of words.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>500+</h1>
+            <p>Articles Published</p>
+          </div>
+          <div className={styles.box}>
+            <h1>1M+</h1>
+            <p>Readers Worldwide</p>
+          </div>
+          <div className={styles.box}>
+            <h1>50+</h1>
+            <p>Expert Contributors</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,22 @@
 import styles from "./contact.module.css";
+import Image from "next/image";
 import React from "react";
 
 const ContactPage = () => {
   return (
-    <h1 className={styles.text}>this is blog page</h1>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+      <Image src="/contact.png" alt="Logo" width="500" height="500" className={styles.image} />
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <textarea placeholder="Message" rows="5"></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
